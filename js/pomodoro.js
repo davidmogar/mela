05724c1c68953ae28b1e24c8d18883bd5c49f081
David Moreno García <david.mogar@gmail.com>
2015-11-18 10:49:19 +0100

    Change style and add fixes

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,34 +1,72 @@
 var currentTask;
+var inBreak = false;
 var tasks = [];
 var timerWorker = new Worker("js/timer-worker.js");
 
 timerWorker.onmessage = function(e) {
   if (e.data.finished) {
-    showNotification((tasks.shift()).description);
-    $('#previous-tasks ol li:first-child').remove();
-    updateListNumbering();
+    if (!inBreak) {
+      showNotification((tasks.shift()).description);
+      $('#previous-tasks ol li:first-child').remove();
+      updateListNumbering();
+    }
+
+    inBreak = false;
+
+    if (tasks.length > 0) {
+      showTimerControls();
+    } else {
+      showFinishMessage();
+    }
   }
-  $("#timer").text(e.data.time);
+
+  $('section#timer-panel p.timer').text(e.data.time);
 }
 
 $(function() {
   Notification.requestPermission();
 
-  $('form').on('submit', function(e) {
+  $('section#break-panel form').on('submit', function(e) {
     e.preventDefault();
 
-    createTask($("#task-description").val(), $("#task-duration").val());
-    $("#task-description").val('');
-    $("#task-duration").val(13);
+    inBreak = true;
+
+    $('section#timer-panel p.task').text('Break');
+    hideBreakPanel();
+    timerWorker.postMessage($("section#break-panel .timer-duration").val());
+    showTimerPanel();
   });
 
-  $('#task-duration').on('input change', function() {
+  $('section#form-panel form').on('submit', function(e) {
+    e.preventDefault();
+
+    createTask($("#task-description").val(), $("section#form-panel .timer-duration").val());
+    resetInputValues();
+  });
+
+  $('input.timer-duration').on('input change', function() {
     var value = $(this).val();
-    $('#duration-label span').text(value + (value > 1? ' minutes' : ' minute'));
+    $(this).siblings('.duration-label').children('span').text(value + (value > 1? ' minutes' : ' minute'));
+  });
+
+  $('button#start').click(function() {
+    $('section#form-panel').addClass('hide');
+    $('section#timer-panel').removeClass('hide');
+    beginTask(tasks[0]);
+  });
+
+  $('#controls button.break').click(function() {
+    hideTimerControls();
+    hideTimerPanel();
+    showBreakPanel();
+  });
+
+  $('#controls button.next').click(function() {
+    beginTask(tasks[0]);
+    hideTimerControls();
   });
 
   $(document.body).on('click', 'span.delete', function() {
-    console.log('hola');
     deleteTask($(this));
   });
 
@@ -38,8 +76,13 @@ $(function() {
 
 });
 
+function beginTask(task) {
+  $('section#timer-panel p.task').text(task.description);
+  timerWorker.postMessage(task.duration);
+}
+
 function createListElement(index, description, duration) {
-  return '<li><span class="number">' + index + '</span>.' + description + ' for ' +
+  return '<li><span class="number">' + index + '.</span>' + description + ' for ' +
       duration + ' minutes<span class="delete">Delete</span></li>';
 }
 
@@ -48,8 +91,6 @@ function createTask(description, duration) {
   $('#previous-tasks ol').append(createListElement(tasks.length + 1,
       description, duration));
 
-  timerWorker.postMessage(duration);
-
   if (storageAvailable('localStorage')) {
     tasks.push({ description: description, duration: duration });
     localStorage.setItem('tasks', JSON.stringify(tasks));
@@ -57,10 +98,22 @@ function createTask(description, duration) {
 }
 
 function deleteTask(element) {
-  var index = element.siblings('span.number').text();
+  var index = element.siblings('span.number').text().substring(0, -1);
   tasks.splice(index - 1, 1);
   element.closest('li').remove();
   updateListNumbering();
+}
+
+function hideBreakPanel() {
+  $('section#break-panel').addClass('hide');
+}
+
+function hideTimerPanel() {
+  $('section#timer-panel').addClass('hide');
+}
+
+function hideTimerControls() {
+  $('#controls').removeClass('show');
 }
 
 function loadPreviousTasks() {
@@ -76,8 +129,26 @@ function loadPreviousTasks() {
 
 function updateListNumbering() {
     $('#previous-tasks ol span.number').each(function(index, element) {
-      $(element).text(index + 1);
+      $(element).text((index + 1) + '.');
     });
+}
+
+function resetInputValues() {
+  $('section#break-panel input.timer-duration').val(4);
+  $('section#break-panel .duration-label span').text('4 minutes');
+  $('section#form-panel input.timer-duration').val(13);
+  $('section#form-panel .duration-label span').text('13 minutes');
+  $('#task-description').val('');
+}
+
+function showBreakPanel() {
+  resetInputValues();
+  $('section#break-panel').removeClass('hide');
+}
+
+function showFinishMessage() {
+  $('section#timer-panel p.task').text('You have completed all your tasks.');
+  $('section#timer-panel p.timer').text('Congratulations!');
 }
 
 function showNotification(task) {
@@ -92,6 +163,15 @@ function showNotification(task) {
       setTimeout(function() { notification.close(); }, 5000);
     }
   }
+}
+
+function showTimerControls() {
+  $('#controls').addClass('show');
+}
+
+function showTimerPanel() {
+  resetInputValues();
+  $('section#timer-panel').removeClass('hide');
 }
 
 function storageAvailable(type) {
